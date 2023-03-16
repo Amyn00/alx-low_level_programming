637e7ed6475cd8f8c1cc94d55e94f87327309527
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-16 08:51:10 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)");
-	printf("Size of an int: 4 byte(s)");
-	printf("Size of a long int: 4 byte(s)");
-	printf("Size of a long long int: 8 byte(s)");
-	printf("Size of a float: 4 byte(s)");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 }
