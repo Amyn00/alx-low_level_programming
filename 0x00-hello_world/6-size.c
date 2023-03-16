24aed8d6bea29d049e665442b223a7f6891271e5
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-16 08:41:28 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various type
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	return (0);
+}
