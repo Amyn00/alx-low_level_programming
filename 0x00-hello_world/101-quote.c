fceb8b6bd1b201e7d51783a8c480ce2c8e32f7ce
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-16 10:44:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	print ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
