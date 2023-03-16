8e35a4a78e74440e1897e0283ec8b3ff72db6db6
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-16 08:25:35 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a string using printf and return new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
