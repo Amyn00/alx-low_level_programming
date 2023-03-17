643d50099afad56f4afd168f666fc5152ad210ed
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-17 15:10:04 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible different
+ * combinations of two digits.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (i != j)
+			{
+				putchar((char) i);
+				putchar((char) j);
+			}
+			if (i == 56 && j == 57)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
