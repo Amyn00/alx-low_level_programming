0318f524ee1c6f50e54fade178c23052f4b39740
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-21 11:14:35 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - write a func that add two int and return result
+ * @a: one number
+ * @b: one number
+ * Return: r
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
