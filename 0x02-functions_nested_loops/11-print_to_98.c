5146357bebaa4049ce6195db97da88196cefe0d9
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-21 11:42:32 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,25 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 99; i++)
+	if (n > 98)
 	{
-		printf("%d", i);
-		if (i < 98)
+		for (i = n; i > 97; i--)
 		{
-			printf(", ");
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	printf("\n");
