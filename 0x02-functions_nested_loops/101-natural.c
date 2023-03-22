83364f8125b878e42476b18f9b7ad71721987f82
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-21 17:33:40 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,12 +15,12 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum = sum + i;
 		}
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
