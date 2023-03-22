1a67a852e6d6461dbc932abaef25a80e5ec99e5f
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-21 17:27:19 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - multiple of 3 and 5 below 1024
+ *
+ * Return: 0;
+ *
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+
+	return (0);
+}
