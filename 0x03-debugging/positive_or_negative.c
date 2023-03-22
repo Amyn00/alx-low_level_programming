0c7137c8cf0519bcfe1229e9d3e087b90e8ad30f
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-22 09:03:28 -0700

    positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - number are pos or neg
+ *
+ * @i: test the number if pos or neg
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is negative\n", i);
+}
