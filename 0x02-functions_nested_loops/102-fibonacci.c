fbb86c4cd6bf06088dc22f2c2baddfb532a001a0
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-21 18:36:07 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 	int i;
-	unsigned long a = 0, b = 1, c;
+	unsigned long a = 1, b = 1, c;
 
-	for (i = 0; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		printf("%lu", a);
 		c = a + b;
