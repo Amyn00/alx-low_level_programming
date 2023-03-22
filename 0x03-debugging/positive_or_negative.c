4f555747661b4cf6337bc153cc00243592103d19
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-22 10:00:05 -0700

    0x03-debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,6 +10,8 @@ void positive_or_negative(int i)
 {
 	if (i > 0)
 		printf("%d is positive\n", i);
-	else
+	else if (i < 0)
 		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
