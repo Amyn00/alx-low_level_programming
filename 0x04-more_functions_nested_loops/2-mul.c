44123fa0b2a56a8bbe04b643f0adb6f3ba16160c
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-23 08:31:09 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - write a func that multiplies two int
+ * @a: num 1
+ * @b: num 2
+ * Return: m
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
