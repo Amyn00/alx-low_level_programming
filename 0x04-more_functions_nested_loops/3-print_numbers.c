ebb5c4a2aaa24bd2029bd140a8b4b6da82f491e9
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-23 08:40:04 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - write a func that print num from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,8 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char);
+void print_numbers(void);
 
 
 #endif
