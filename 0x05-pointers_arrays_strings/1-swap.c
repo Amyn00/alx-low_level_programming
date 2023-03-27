04c996dbcc5ca320199686c1a1039a898b987689
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-03-27 07:21:23 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - write a func that swap the value of two ints
+ *
+ * @a: input as pointeur a
+ * @b: input as pointeur b
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
