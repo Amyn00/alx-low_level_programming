7821db7d1239057eecd58913590f4d468678154d
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-03 15:58:52 -0700

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - write a func that sets the value of a pointer
+ * to char
+ *
+ * @s: input
+ * @to: input
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
