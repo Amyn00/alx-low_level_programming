0ae60ade7ad87c81de09b957929b8364979a0dad
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-05 17:27:58 -0700

    _palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _lenght - get lenght
+ * @s: input
+ * Return: lenght
+ */
+
+int _lenght(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (1 + _lenght(++s));
+}
+
+/**
+ * _isPalindrome - palindrome reverse
+ * @c: input
+ * @len: input
+ * Return: 1 or 0
+ */
+
+int _isPalindrome(char *s, int len)
+{
+	if(*s == *(s + 1))
+	{
+		if (len <= 0)
+			return (1);
+		else
+			return (_isPalindrome(++s, len - 1));
+	}
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - write a func that return 1 if a string
+ * is a palindrome and 0 if not
+ * @s: input
+ * Return: 1 if is a palindrome 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int _isPalindrome(char *s, int len);
+	int _lenght(char *s);
+	int len;
+
+	len = _lenght(s) - 1;
+	return (_isPalindrome(s, --len));
+}
