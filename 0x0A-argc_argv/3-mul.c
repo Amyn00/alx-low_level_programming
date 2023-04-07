70b4bfc7c6111a9e748b4769dfecb6e6d1009510
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-07 08:54:37 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,17 @@ int main(int argc, char *argv[])
 {
 	int x, y, mul;
 
-	if (argc > 1 && argc < 4)
-	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-		mul = x * y;
-		printf("%d\n", mul);
-
-		return (0);
-	}
-	else
+	if ( argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
+
+	printf("%d\n", mul);
+
+	return (0);
 }
