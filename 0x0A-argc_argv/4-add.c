5cd7df9d72c01cd94dd9e4ae9ec50ee82805ff7c
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-07 10:14:00 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - write a prog that adds + num
+ * @argc: count
+ * @argv: value
+ * Return: 0 and 1 if the arg is not number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 int _putchar(char);
 
 #endif
