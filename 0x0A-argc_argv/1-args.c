ac08cbe594937e3fb1640d2bb8631f0d77b34eb3
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-07 07:46:39 -0700

    args number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - write a prog that print the num of arg passed into it
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
