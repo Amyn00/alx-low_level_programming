9be7976e3a9bb868ef4e1a735144007615e784c5
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-11 07:53:06 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, tot_space;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
