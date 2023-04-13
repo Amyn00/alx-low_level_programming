74245035c7873ed4ce968bd493857efed0b13eeb
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-13 15:30:15 -0700

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * array_range - write a funct that creat an array of int
+ * @min: input
+ * @max: input
+ * Return: array
+ */
+
+int *array_range(int min, int max)
+{
+	int *p, i;
+
+	if (min > max)
+		return (NULL);
+	for (i = min; i <= max; i++)
+		;
+	p = malloc(sizeof(int) * i);
+	if (p == NULL)
+		return (NULL);
+	for (i = min; i <= max; i++)
+		p[i - min] = i;
+	return (p);
+}
