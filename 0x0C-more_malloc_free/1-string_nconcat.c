9eb99dce043cd6f9cdf2768e1ef1746a2acc987a
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-13 14:10:05 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,35 @@
 #include "main.h"
 
 /**
- * string_nconcat - write a func that concat two str
+ * string_nconcat - concat two string
  * @s1: input
  * @s2: input
  * @n: input
- * Return: conct or NULL if fails
+ * Return: conct
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int l1, i, x;
 	char *conct;
-	unsigned int l1 = 0, l2 = 0, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
-	else
-		while (s1[l1])
-			l1++;
 	if (s2 == NULL)
 		s2 = "";
-	else
-		while (s2[l2])
-			l2++;
-	conct = malloc((sizeof(*conct) * (l1 + l2)) + 1);
+	l1 = 0;
+	while (s1[l1])
+		l1++;
+	conct = malloc(sizeof(*conct) * l1 + n + 1);
 	if (conct == NULL)
 		return (NULL);
-	for (i = 0; i < l1; i++)
-		*(conct + i) = *(s1 + i);
-	if (n >= l2)
-		for (j = 0; j < l2; j++)
-			*(conct + i + j) = *(s2 + j);
-	else
-		for (j = 0; j < n; j++)
-			*(conct + i + j) = *(s2 + j);
-	*(conct + i + j) = '\0';
+	for (i = 0, x = 0; i < (l1 + n); i++)
+	{
+		if (i < l1)
+			conct[i] = s1[i];
+		else
+			conct[i] = s2[x++];
+	}
+	conct[i] = '\0';
 	return (conct);
 }
