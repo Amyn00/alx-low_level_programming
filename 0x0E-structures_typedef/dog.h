bbc279479e61a6f863427d4c93e3c278f257871b
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-17 07:50:33 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - my first test stuct
+ * @name: the name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: this struct is an typdef of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
