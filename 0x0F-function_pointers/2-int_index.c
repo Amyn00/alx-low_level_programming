f61db9f7fb7df5e2370cb77afb45ce977a1e3825
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-19 09:44:27 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,21 +10,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	if (size <= 0)
-		return (-1);
-
-	if (array != NULL && cmp == NULL && size > 0)
+	if (size > 0)
 	{
-		while (i < size)
-		{
-			if (cmp(array[i]))
+		if (array != NULL && cmp != NULL)
+			while (i < size)
 			{
-				return (i);
+				if (cmp(array[i]))
+					return (i);
+				i++;
 			}
-			i++;
-		}
 	}
 	return (-1);
 }
