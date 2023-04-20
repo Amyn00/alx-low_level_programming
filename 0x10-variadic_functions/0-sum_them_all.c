cc1c18f78a8d95bb81a43ee34da64350c8bbd988
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-20 09:04:19 -0700

    sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	va_start (ap, n);
+	va_start(ap, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum =+ va_arg (ap, int);
-	va_end (ap);
+		sum += va_arg(ap, int);
+	va_end(ap);
 	return (sum);
 }
