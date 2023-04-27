c877bd1602eb48dd621c549da9632074aaedb105
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-27 06:01:32 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@ size_t print_list(const list_t *h)
 {
 	int n = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		n++;
 		h = h->next;
 	}
