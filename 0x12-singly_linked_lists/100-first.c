0e3171368ed9fcba5fa66b2551eed976affa6b86
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-27 08:06:26 -0700

    print_before_main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * str - write a funct that print ... before the main func is executed
+ */
+void main_0(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,4 +30,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+void main_0(void) __attribute__((constructor));
+
 #endif
