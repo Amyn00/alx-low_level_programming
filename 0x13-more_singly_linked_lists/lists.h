f026e87a410272893c12cc021d573a2ca44448d8
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-30 02:16:07 -0700

    print_list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,9 +12,9 @@ int _putchar(char);
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- * 
+ *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
