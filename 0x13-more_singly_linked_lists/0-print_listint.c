20832460f2e8fec018a0d84e8fc39625c10b279e
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-30 00:51:05 -0700

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
 	}
-	return (num);
+	return (nodes);
 }
