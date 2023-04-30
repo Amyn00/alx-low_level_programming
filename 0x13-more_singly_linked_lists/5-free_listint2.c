8be2394e309edd30c0cc085264f886e9a0b45c6b
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-04-30 01:27:51 -0700

    free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = temp;
 	}
+	head = NULL;
 }
