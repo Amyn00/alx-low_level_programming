e24e278072e243e1df73fee92a111c3cd9f7d179
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-05-01 05:24:42 -0700

    printint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,45 @@
 #include "lists.h"
+/**
+ * looped_listint - write func that count the num of unique nodes
+ * @head: input
+ * Return: num of unique nodes, otherwise 0 or exit 98
+ */
+
+size_t looped_listint(const listint_t *head)
+{
+	const listint_t *h1, *h2;
+	size_t node = 1;
+
+	if (head == NULL || head->next == NULL)
+		exit(98);
+	h1 = head->next;
+	h2 = head->next->next;
+
+	while (h2)
+	{
+		if (h1 == h2)
+		{
+			h1 = head;
+			while (h1 != h2)
+			{
+				node++;
+				h1 = h1->next;
+				h2 = h2->next;
+			}
+			h1 = h1->next;
+			while (h1 != h2)
+			{
+				node++;
+				h1 = h1->next;
+			}
+			return (node);
+		}
+		h1 = h1->next;
+		h2 = h2->next->next;
+	}
+	return (0);
+}
+
 
 /**
  * print_listint_safe - write a func that print a listint_t linked list
@@ -8,32 +49,24 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n = 0;
-	const listint_t *h1, *h2;
+	size_t n = 0, node = looped_listint(head);
 
-	if (head == NULL)
-		exit(98);
-	h1 = head;
-	h2 = head->next;
-	while (h2 != NULL && h2 > h1)
+	if (node == 0)
 	{
-		printf("[%p] %d\n", (void *) h1, h1->n);
-		n++;
-		h2 = h2->next;
-		h1 = h1->next;
-		if (h1 != NULL && h2 > h1)
+		for (; head != NULL; node++)
 		{
-			printf("[%p] %d\n", (void *) h1, h1->n);
-			n++;
-			h1 = h1->next;
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next;
 		}
 	}
-	printf("[%p] %d\n", (void *) h1, h1->n);
-	n++;
-	if (h2 != NULL)
+	else
 	{
-		printf("-> [%p] %d\n", (void *) h2, h2->n);
-		exit(98);
+		for (; n < node; n++)
+		{
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *) head, head->n);
 	}
-	return (n);
+	return (node);
 }
