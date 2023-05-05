0b903405cd91ea48779bbad14fb421653a547d2a
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-05-04 18:11:42 -0700

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int num;
+	unsigned int num = 0;
 
 	if (b == NULL)
 		return (0);
-	num = 0;
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-		       return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		num *= 2;
 		if (b[i] == '1')
 			num++;
