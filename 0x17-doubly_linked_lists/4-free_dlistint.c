009ccba7397804bc651096acc575488aae9b4abc
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-17 07:18:08 -0700

    task 4 done

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp = head;
+	dlistint_t *tmp;
 
 	if (head != NULL)
 		while (head->prev != NULL)
 			head = head->prev;
-	while (tmp != NULL)
+	while ((tmp = head) != NULL)
 	{
 		head = head->next;
 		free(tmp);
